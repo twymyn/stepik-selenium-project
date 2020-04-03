3fad92d4ad0088291eb23812966e39c8866439af
Tina <lc.tinatran@gmail.com>
2020-04-03 18:51:23 +0200

    Added check for the login page redirect

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -13,3 +15,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
