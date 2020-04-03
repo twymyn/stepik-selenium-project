85ab0799534ad02667fce9178d84426df3995dc1
Tina <lc.tinatran@gmail.com>
2020-04-03 20:01:42 +0200

    Parameterized the test

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,16 @@
 import pytest
 from pages.product_page import ProductPage
 
-links = ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
-         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"]
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+         pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
 @pytest.mark.parametrize('link', links)
