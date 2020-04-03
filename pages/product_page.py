59317ee91fd36127e6c6474ac2b56734c6f06874
Tina <lc.tinatran@gmail.com>
2020-04-03 19:59:43 +0200

    Fixed the check to a strict match

--- pages/product_page.py
+++ pages/product_page.py
@@ -17,7 +17,7 @@ class ProductPage(BasePage):
     def should_be_product_in_basket(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         success_product_in_basket = self.browser.find_element(*ProductPageLocators.SUCCESS_PRODUCT_IN_BASKET).text
-        assert product_name in success_product_in_basket, "Product was not added to the basket"
+        assert product_name == success_product_in_basket, "Product was not added to the basket"
 
     def should_be_right_basket_total(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
